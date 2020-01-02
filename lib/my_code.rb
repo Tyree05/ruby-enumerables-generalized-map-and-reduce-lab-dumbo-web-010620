022aa54abb7db0120e9e2994389c1992e2771dcf
Tyree Williams <tyreewilliams515@gmail.com>
2020-01-02 06:19:19 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
-# Your Code Here
+def map(element)
+  array_new = []
+  index = 0
+    while index < element.length do
+      array_new << yield(element[index])
+      index += 1
+    end
+    array_new
+end
+
+def reduce(array, start = nil)
+  if start
+    total = start
+    index = 0
+  else
+    total = array[0]
+    index = 1
+  end
+  while index < array.length do
+    total = yield(total, array[index])
+    index += 1
+  end
+  total
+end
