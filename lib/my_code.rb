99667655155783ce9c077f5f84d62a117183238a
Tyree Williams <tyreewilliams515@gmail.com>
2020-01-02 05:57:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,9 +11,14 @@ end
 def reduce(array, start = 0)
   index = 0
   total = start
+  result = true
   while index < array.length do
+    if array[index == false] do
+      result = false
+    end
     total += array[index]
     index += 1
   end
   total
+  return result
 end
