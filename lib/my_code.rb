1d92475844a06de7aa28b05054cc0c7dba44b93e
Tyree Williams <tyreewilliams515@gmail.com>
2020-01-02 05:35:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,6 +1,9 @@
 def map(element)
   array_new = []
-  yield(element)
-  array_new << element
-  array_new
+  index = 0
+    while index < element.length do
+      array_new << yield(element[index])
+      index += 1
+    end
+    array_new
 end
