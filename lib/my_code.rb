3342f600ac7b4467f5bba64055dedba650599c12
Tyree Williams <tyreewilliams515@gmail.com>
2020-01-02 05:43:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,3 +8,12 @@ def map(element)
     array_new
 end
 
+def reduce(array, start = 0)
+  strt_pt = start
+  total = 0
+  while strt_pt < array.length do
+    total += array[strt_pt]
+    strt_pt += 1
+  end
+  total
+end
